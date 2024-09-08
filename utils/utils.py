1a10533536cfc0397b577e63f40303930b48e1a4
maartenroose <141149174+maartenroose@users.noreply.github.com>
2024-09-09 00:43:46 +0200

    error analysis

--- utils/utils.py
+++ utils/utils.py
@@ -1,13 +1,25 @@
 import pandas as pd
+import gzip
 
 def read_vcf(file: str) -> pd.DataFrame:
     num_header = 0
-    with open(file) as f:
-        for line in f.readlines():
-            if line.startswith("##"):
-                num_header += 1
-            else:
-                break
+
+    if file.endswith(".gz"):
+        with gzip.open(file, "rt") as f:
+            for line in f.readlines():
+                if line.startswith("##"):
+                    num_header += 1
+                else:
+                    break
+
+    else:
+        with open(file) as f:
+            for line in f.readlines():
+                if line.startswith("##"):
+                    num_header += 1
+                else:
+                    break
+
     vcf = pd.read_csv(file, sep="\t", skiprows=num_header)
     vcf = vcf.rename({"#CHROM": "CHROM"}, axis=1)
     return vcf
